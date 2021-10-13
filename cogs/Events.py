df7824af40cd98f5debdb2202940017e18a2e954
ZI1E <teammohmmed77@gmail.com>
2021-10-13 10:52:50 +0300

    Added Events Cog ⚠

--- cogs/Events.py
+++ cogs/Events.py
@@ -1,4 +1,5 @@
 import nextcord
+import json
 
 from nextcord.ext import commands
 from rich.console import Console
@@ -6,24 +7,21 @@ from rich.table import Table
 
 console = Console()
 
+settings_file = json.load(open('settings.json'))
+
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-    @commands.command()
-    async def ping(ctx):
-        await ctx.reply('Pong !')
-
     @commands.Cog.listener()
-    async def on_ready():
+    async def on_ready(self):
         # Our custom print
         table = Table()
         table.add_column("[bold white]Bot info[/bold white]", justify="left", style="bold blue", no_wrap=True)
         table.add_row(f"[bold yellow]Alert:[/bold yellow] Everything Working Well")
         console.print(table)
 
-        await bot.change_presence(
+        await self.bot.change_presence(
             activity=nextcord.Game(name=settings_file['activity']), # Changing bot activity
             status=settings_file['status'] # Bot status // ["idle", "dnd", "online"]
         ) 
