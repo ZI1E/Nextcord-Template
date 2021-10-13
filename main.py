113e20f6e10e6da030a81ea4f94d9f5a7ddc5539
ZI1E <teammohmmed77@gmail.com>
2021-10-13 08:05:51 +0300

    Added CCustom Status And Activity 🧾

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import nextcord
 import json
 
 from nextcord.ext import commands
@@ -14,9 +15,17 @@ bot = commands.Bot(command_prefix=settings_file['prefix'])
 async def ping(ctx):
     await ctx.reply('Pong !')
 
-table = Table()
-table.add_column("[bold white]Bot info[/bold white]", justify="left", style="bold blue", no_wrap=True)
-table.add_row(f"[bold yellow]Alert:[/bold yellow] Everything Working Well")
-console.print(table)
+@bot.event
+async def on_ready():
+    # Our custom print
+    table = Table()
+    table.add_column("[bold white]Bot info[/bold white]", justify="left", style="bold blue", no_wrap=True)
+    table.add_row(f"[bold yellow]Alert:[/bold yellow] Everything Working Well")
+    console.print(table)
+
+    await bot.change_presence(
+        activity=nextcord.Game(name="!help"), # Changing bot activity
+        status=nextcord.Status.dnd # Bot status // ["idle", "dnd", "online"]
+    ) 
 
 bot.run(settings_file['token'])
