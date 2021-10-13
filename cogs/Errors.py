552aa8a13e1654b7dace86a82c08af1eef7fbf8a
ZI1E <teammohmmed77@gmail.com>
2021-10-13 09:12:07 +0300

    A Simple Errors Handler ❌

---
+++ cogs/Errors.py
@@ -0,0 +1,29 @@
+from nextcord.ext import commands
+from nextcord.ext.commands import errors
+
+class Errors(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, err):
+        if isinstance(err, errors.MissingRequiredArgument) or isinstance(err, errors.BadArgument):
+            helper = str(ctx.invoked_subcommand) if ctx.invoked_subcommand else str(ctx.command)
+            await ctx.send_help(helper)
+
+        elif isinstance(err, errors.CheckFailure):
+            pass
+
+        elif isinstance(err, errors.MaxConcurrencyReached):
+            await ctx.reply("((You've reached max capacity of command usage at once, please finish the previous one...**")
+
+        elif isinstance(err, errors.CommandOnCooldown):
+            await ctx.reply(f"**This command is on cooldown... try again in `{err.retry_after:.2f}`` seconds.**")
+
+        elif isinstance(err, errors.CommandNotFound):
+            await ctx.reply(f"**This command is not found... try again**")
+
+
+def setup(bot):
+    bot.add_cog(Errors(bot))
