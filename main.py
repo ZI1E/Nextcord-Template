d37485da0e24d0b72c4ff44533782495212b4f90
ZI1E <teammohmmed77@gmail.com>
2021-10-13 07:48:43 +0300

    Added Custom Print 🖨

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import json
+
 from nextcord.ext import commands
+from rich import table
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
 
 settings_file = json.load(open('settings.json'))
 bot = commands.Bot(command_prefix=settings_file['prefix'])
@@ -7,5 +13,10 @@ bot = commands.Bot(command_prefix=settings_file['prefix'])
 @bot.command()
 async def ping(ctx):
     await ctx.reply('Pong !')
+
+table = Table()
+table.add_column("[bold white]Bot info[/bold white]", justify="left", style="bold blue", no_wrap=True)
+table.add_row(f"[bold yellow]Alert:[/bold yellow] Everything Working Well")
+console.print(table)
 
 bot.run(settings_file['token'])
