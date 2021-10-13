979cdec3c874943ef168d1f54d93ce986d868ebe
ZI1E <teammohmmed77@gmail.com>
2021-10-13 09:15:36 +0300

    Moved Some Events To Cogs ✅

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import os
 
 from pathlib import Path
 from nextcord.ext import commands
-from rich.console import Console
-from rich.table import Table
 
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
-console = Console()
 
 def get_prefix(bot, message):
     with open('prefixes.json', 'r') as f:
@@ -18,19 +15,6 @@ def get_prefix(bot, message):
 
 settings_file = json.load(open('settings.json'))
 bot = commands.Bot(command_prefix=(get_prefix))
-
-@bot.event
-async def on_ready():
-    # Our custom print
-    table = Table()
-    table.add_column("[bold white]Bot info[/bold white]", justify="left", style="bold blue", no_wrap=True)
-    table.add_row(f"[bold yellow]Alert:[/bold yellow] Everything Working Well")
-    console.print(table)
-
-    await bot.change_presence(
-        activity=nextcord.Game(name=settings_file['activity']), # Changing bot activity
-        status=settings_file['status'] # Bot status // ["idle", "dnd", "online"]
-    ) 
 
 @bot.event
 async def on_guild_join(guild): # Add default if bot join any guild
