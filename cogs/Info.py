cc3e2ebef315302cd95a59dab9e55174d7f1d069
ZI1E <teammohmmed77@gmail.com>
2021-10-13 09:03:01 +0300

    Added Cogs Commands ✅

---
+++ cogs/Info.py
@@ -0,0 +1,15 @@
+import nextcord
+from nextcord.ext import commands
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    async def ping(ctx):
+        await ctx.reply('Pong !')
+
+
+def setup(bot):
+    bot.add_cog(Info(bot))
